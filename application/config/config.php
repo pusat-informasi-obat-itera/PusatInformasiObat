219dbcd3bad5cdcf24f01fcc4c72c1ca9e643b54
Aansanova <48778047+Aansanova@users.noreply.github.com>
2019-03-26 17:52:17 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/templateitera';
-$config['base_url'] = 'http://localhost/piora';
+$config['base_url'] = 'http://localhost/PusatInformasiObat';
 /*
 |--------------------------------------------------------------------------
 | Index File
